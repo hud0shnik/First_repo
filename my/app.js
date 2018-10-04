2ccb4b6f0c42d782ca043adbad62b0163b21cd9e
Danya Egorov <c4nsero@mail.ru>
2018-10-04 16:29:13 +0300

    -4-

--- my/app.js
+++ my/app.js
@@ -1,7 +1,10 @@
 var main = function () {
 "use strict";
 $(".comment-input button").on("click", function (event) {
-console.log("Hello, World!");
+var $new_comment = $("<p>"),
+comment_text = $(".comment-input input").val();
+$new_comment.text(comment_text);
+$(".comments").append($new_comment);
 });
 };
 $(document).ready(main);
