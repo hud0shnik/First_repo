2c51219a6a6b5c53bf7994cfae2f9083530b44ae
Danya Egorov <c4nsero@mail.ru>
2019-05-23 21:23:03 +0300

    i23052019_2

--- cards/main.js
+++ cards/main.js
@@ -1,25 +1,20 @@
 const cards = document.querySelectorAll('.card');
 
 for (let i = 0; i < cards.length; i++) {
-	const card = cards[i];
+	let card = cards[i];
 	card.addEventListener('mousemove', startRotate );
 	card.addEventListener('mouseout', stopRotate );
 }
 
 function startRotate(event) {
-const cardItem =  this.querySelector('.card-item');
-const halfH = cardItem.offsetHeight / 2;
-const halfW = cardItem.offsetWidth / 2;
-cardItem.style.transform = 'rotateX('+ - (event.offsetY - halfH) / 5 +'deg) rotateY(' + (event.offsetX - halfW) / 5 + 'deg)'
-
+let cardItem =  this.querySelector('.card-item');
+let halfH = cardItem.offsetHeight / 2;
+let halfW = cardItem.offsetWidth / 2;
+cardItem.style.transform = 'rotateX('+ - (event.offsetY - halfH) / 5 +'deg) rotateY(' + (event.offsetX - halfW) / 6 + 'deg)'
 }
 function stopRotate(event) {
 	const cardItem = this.querySelector('.card-item');
 	cardItem.style.transform = 'rotate(0)';
-
-
-
-
 }
 
  
